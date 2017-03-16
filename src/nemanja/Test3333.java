0d831624bb231dbe07a8994bc23d6847648953ec
ntesanovic <ntesanovic@student.tgm.ac.at>
2017-03-16 10:54:41 +0100

    ssh Addresse als Kommentar hinzugefügt

--- src/nemanja/Test3333.java
+++ src/nemanja/Test3333.java
@@ -1,5 +1,5 @@
 package nemanja;
-
+//git@github.com:ntesanovic/Test3333.git
 public class Test3333 {
 	public static void main(String[] args){
 		System.out.println("Guten Morgen Hr. Tesanovic :)");
